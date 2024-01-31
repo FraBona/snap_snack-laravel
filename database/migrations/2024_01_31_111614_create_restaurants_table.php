b4b5bc441ef47a98bef1ad5412d7056ba8e2b44e
FraBona <francescobonafede08@gmail.com>
2024-01-31 12:48:05 +0100

    ADD migrations restaurant and category and table pivot

---
+++ database/migrations/2024_01_31_111614_create_restaurants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('address');
+            $table->string('phone_number')->unique();
+            $table->string('vat')->unique();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
