22d28de29a3247577ed8e11e232f571ca2815334
LucaLF-dev <139238422+LucaLF-dev@users.noreply.github.com>
2024-01-31 12:56:24 +0100

    Add - dish and order table migration

--- database/migrations/2024_01_31_112253_create_dishes_table.php
+++ database/migrations/2024_01_31_112253_create_dishes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description')->nullable();
             $table->decimal('price');
-            $table->boolean('visible')->default('true')->nullable();
+            $table->boolean('visible')->default(true)->nullable();
             $table->string('photo')->nullable();
             $table->timestamps();
         });
