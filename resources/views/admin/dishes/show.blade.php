af4c5139b3c5ef358c9d1a061f912ed34c0d83c4
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-02-06 16:36:07 +0100

    ADD - show style for the plate

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,4 +1,9 @@
 <style>
+    .container {
+        display: flex;
+        justify-content: center;
+    }
+
     .img-dish {
         min-width: 200px;
         min-height: 150px;
@@ -8,28 +13,60 @@
         border-radius: 1rem;
     }
 
+    .dish-show {
+        height: calc(100vh - 76px);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
     .dish-title {
-      font-size: 24px;
-      font-weight: 800;
+        font-size: 24px;
+        font-weight: 800;
+    }
+
+    .ul-resetted {
+        display: block;
+        list-style-type: disc;
+        margin-block-start: 0em !important ;
+        margin-block-end: 0em !important ;
+        margin-inline-start: 0px !important ;
+        margin-inline-end: 0px !important;
+        padding-inline-start: 0px !important;
+    }
+    .dish-wrapper {
+        background-color: white;
+        min-width: 200px;
+        max-width: 600px;
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        border-radius: 1rem ;
+        box-shadow: rgba(128, 128, 128, 0.44) -6px -6px 6px -6px;
     }
 </style>
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-      <img class="img-dish mt-5" src="{{ asset('storage/' . $dish->photo) }}" alt="photo-restaurant">
-      <ul>
-        <li class="dish-title"><strong>{{ $dish->name }}</strong></li>
-        <li><strong>{{ $dish->description }}</strong></li>
-        <li><strong>{{ $dish->price }}</strong></li>
-      </ul>
-      <form action="{{ route('admin.dishes.destroy', $dish) }}" method="post">
-          @csrf
-          @method('DELETE')
-  
-          <input class="btn btn-danger" type="submit" value="Cancella"
-              onclick="return confirm('sei sicuro di voler cancellare?')" class="btn btn-danger">
-      </form>
-      <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Torna al tuo Menu</a>
-    </div>
+    <section class="dish-show">
+        <div class="container">
+            <div class="dish-wrapper">
+                <img class="img-dish mt-5" src="{{ asset('storage/' . $dish->photo) }}" alt="">
+                <ul class="ul-resetted">
+                    <li class="dish-title text-center mt-4"><strong>{{ $dish->name }}</strong></li>
+                    <li class="text-center mt-4"><strong>{{ $dish->description }}</strong></li>
+                    <li class="text-center mt-4"><strong>{{ $dish->price }}</strong></li>
+                </ul>
+                <form action="{{ route('admin.dishes.destroy', $dish) }}" method="post">
+                    @csrf
+                    @method('DELETE')
 
+                    <input class="btn btn-danger mt-4" type="submit" value="Cancella"
+                        onclick="return confirm('sei sicuro di voler cancellare?')" class="btn btn-danger">
+                </form>
+                <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Torna al tuo Menu dei Piatti</a>
+            </div>
+        </div>
+    </section>
 @endsection
