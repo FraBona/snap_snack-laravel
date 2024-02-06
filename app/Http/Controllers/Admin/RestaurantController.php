4ddfb4911b785dc685f9f46bd2dfcaa205cf3167
FraBona <francescobonafede08@gmail.com>
2024-02-06 17:10:31 +0100

    validation phone number

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -51,7 +51,7 @@ class RestaurantController extends Controller
             $request->validate([
                 'name' => 'required|regex:/[a-zA-Z\s]+/|min:3|max:30|string',
                 'address' => 'required|min:10|max:255|string',
-                'phone_number' => 'required|regex:/[0-9]+/|min:6|max:30|string',
+                'phone_number' => 'required|regex:/[0-9]+/|min:9|max:10|string',
                 'vat' => 'required|max:11|min:11|string',
                 'photo' => 'nullable|image|mimes:jpeg,png,pdf|max:2048',
                 'user_id' => 'nullable|exists:users,id',
