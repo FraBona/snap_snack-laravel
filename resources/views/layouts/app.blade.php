1dc40a90f9a5c1f727fa7cb10a99397ba0e48e28
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-02-08 17:17:49 +0100

    ADD- locale minor adjust

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,11 +76,11 @@
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item"
                                             href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
-                                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profilo') }}</a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
+                                            {{ __('Esci') }}
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
