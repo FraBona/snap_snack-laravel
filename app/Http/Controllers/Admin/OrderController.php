e3bb1e47be522f583b4fa74818d47e5184875098
FraBona <francescobonafede08@gmail.com>
2024-02-12 17:59:01 +0100

    add show

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use App\Models\Dish;
+use App\Models\DishOrder;
 use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\Auth;
@@ -46,7 +47,22 @@ class OrderController extends Controller
      */
     public function show(Order $order)
     {
-        //
+        $user = Auth::user()->id;
+        $restaurant = Restaurant::where('user_id', '=', $user)->first();
+        if ($restaurant) {
+            $dishes = Dish::where('restaurant_id', '=', $order->restaurant_id)->get();
+            $quantities = DishOrder::where('order_id', '=', $order->id)->get();
+            $dishesWithQuantities = [];
+            foreach ($dishes as $dish) {
+                $dishQuantity = $quantities->where('dish_id', $dish->id)->first();
+            
+                $dishesWithQuantities []= [
+                    'name' => $dish->name,
+                    'quantity' => $dishQuantity ? $dishQuantity->quantity : 0, 
+                ];
+            }
+            return view('admin.orders.show', compact('order', 'dishesWithQuantities'));
+        }
     }
 
     /**
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<section class="order-wrapper">
+<section class="order-wrapper mt-5">
   <div class="container">
-    <div class="row">
+    <div class="row gy-3 gap-3 flex-wrap justify-content-center">
       @foreach ($orders as $order)
-        <div class="col-md-12 ">
-          <ul class="d-flex gap-2 ul-ordini justify-content-center">
-            <li>Nome: {{$order->customer_name}}</li>
-            <li>Cognome: {{$order->customer_last_name}}</li>
-            <li>Email: {{$order->customer_email}}</li>
-            <li>Indirizzo: {{$order->customer_address}}</li>
-            <li>Telefono: {{$order->customer_phone}}</li>
-            <li>Totale: {{$order->amount}}</li>
+      <a href="{{route ('admin.orders.show', $order)}}" class="col-md-3 py-4 ancor-order">
+        <div class="">
+          <ul class="d-flex flex-column gap-2 ul-ordini justify-content-center">
+            <li class="d-flex"><strong>Nome: </strong> {{$order->customer_name}}</li>
+            <li class="d-flex"><strong>Cognome: </strong> {{$order->customer_last_name}}</li>
+            <li class="d-flex"><strong>Email: </strong> {{$order->customer_email}}</li>
+            <li class="d-flex"><strong>Indirizzo: </strong> {{$order->customer_address}}</li>
+            <li class="d-flex"><strong>Telefono: </strong> {{$order->customer_phone}}</li>
+            <li class="d-flex"><strong>Totale: </strong> {{$order->amount}} &euro;</li>
           </ul>
         </div>
+      </a>
     @endforeach
     </div>
   </div>
@@ -30,10 +32,37 @@
     justify-content: center;
     align-items: center; 
     width: 100%;
-    height: calc(100vh - 200px);
   }
 
   .ul-ordini > li{
-    text-align: center;
+    display: flex;
+    justify-content: space-between
+  }
+
+  .col-md-3{
+    background-color: white;
+    border-radius: 1rem;
+    
+  }
+
+  .col-md-3:hover{
+    border: 1px solid black;
+    cursor: pointer;
+  }
+
+  .ancor-order{
+    text-decoration: none;
+    color: black;
+  }
+
+  .ancor-order:link{
+    text-decoration: none;
+  }
+  .ancor-order:visited{
+    text-decoration: none;
+  }
+
+  .ancor-order:hover{
+    text-decoration: none;
   }
 </style>
