dcab613539c4930e3fdb25d04bc7d33cd40f5963
FraBona <francescobonafede08@gmail.com>
2024-02-14 15:21:34 +0100

    add show index of order and stats for order

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,71 +1,63 @@
+
 @extends('layouts.app')
 @section('content')
-<section class="order-wrapper mt-5">
-  <div class="container">
-    <div class="row gy-3 gap-3 flex-wrap justify-content-center">
-      @foreach ($orderAmount as $order)
-      <?php
-        $object = (object)$order;
-      ?>
-      <a href="{{route ('admin.orders.show', $order)}}" class="col-md-3 py-4 ancor-order">
-        <div class="">
-          <ul class="d-flex flex-column gap-2 ul-ordini justify-content-center">
-            <li class="d-flex"><strong>Nome: </strong> {{$object->order->customer_name}}</li>
-            <li class="d-flex"><strong>Cognome: </strong> {{$object->order->customer_last_name}}</li>
-            <li class="d-flex"><strong>Email: </strong> {{$object->order->customer_email}}</li>
-            <li class="d-flex"><strong>Indirizzo: </strong> {{$object->order->customer_address}}</li>
-            <li class="d-flex"><strong>Telefono: </strong> {{$object->order->customer_phone}}</li>
-            <li class="d-flex"><strong>Totale: </strong> {{$object->amount}} &euro;</li>
-          </ul>
+    <h2 class="text-center pt-5">
+        I miei Ordini
+    </h2>
+    <section class="order-wrapper mt-5">
+        <div class="container">
+            <table class="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome Completo</th>
+                        <th scope="col">Indirizzo</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Totale</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                    {{-- @dd($order) --}}
+                    <?php
+                      $object = (object)$order;
+                    ?>
+                        <tr class="ordine-row" id="show"
+                            onclick="redirectShow('{{ route('admin.orders.show', $order) }}')">
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $object->customer_name }} {{ $order->customer_last_name }}</td>
+                            <td>{{ $object->customer_address }}</td>
+                            <td>{{$object->created_at}}</td>
+                            <td>{{ $object->amount }} &euro;</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-      </a>
-    @endforeach
-    </div>
-  </div>
-</section>
+    </section>
 @endsection
-
+ 
 <style>
-  .ul-ordini{
-    list-style: none;
-  }
-
-  .order-wrapper{
-    display: flex;
-    justify-content: center;
-    align-items: center; 
-    width: 100%;
-  }
-
-  .ul-ordini > li{
-    display: flex;
-    justify-content: space-between
-  }
-
-  .col-md-3{
-    background-color: white;
-    border-radius: 1rem;
-    
-  }
-
-  .col-md-3:hover{
-    border: 1px solid black;
-    cursor: pointer;
-  }
-
-  .ancor-order{
-    text-decoration: none;
-    color: black;
-  }
-
-  .ancor-order:link{
-    text-decoration: none;
-  }
-  .ancor-order:visited{
-    text-decoration: none;
-  }
-
-  .ancor-order:hover{
-    text-decoration: none;
-  }
+    .order-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+ 
+    .thead {
+        background-color: #212529;
+    }
+ 
+    .ordine-row:hover {
+        transform: scale(1.03);
+        cursor: pointer;
+    }
 </style>
+ 
+ 
+<script>
+    function redirectShow(destinationPage) {
+        window.location.href = destinationPage
+    }
+</script>
--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -6,42 +6,55 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
 <script>
-  const ctx = document.getElementById('myChart');
-  // const months = [
-  //   'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
-  //   'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
-  // ];
-  const currentYear = new Date().getFullYear();
-  const years = [];
+    const ctx = document.getElementById('myChart');
 
-  for (let i = 0; i < 0; i++) {
-    const year = currentYear - i;
-    for (let month = 1; month <= 12; month++) {
-      years.push({ year: year, month: month });
-    }
-  }
+    // Estrai i dati appropriati dall'array associativo
+    const dataCounts = Object.values(@json($ordersData)).map(item => item.orderCount);
+    const dataAmounts = Object.values(@json($ordersData)).map(item => item.totalSalesAmount);
+    const labels = Object.keys(@json($ordersData));
 
-
-  let labels = @json($orderCountsByMonth);
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: years,
-      datasets: [{
-        label: '# of Votes',
-        data: labels,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ordini per mese',
+                data: dataCounts,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)', // Colore per il conteggio degli ordini
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                yAxisID: 'orders' // Assegna un ID all'asse Y per il conteggio degli ordini
+            }, {
+                label: 'Ammontare per mese',
+                data: dataAmounts,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)', // Colore per l'ammontare degli ordini
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+                yAxisID: 'sales' // Assegna un ID all'asse Y per l'ammontare delle vendite
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{ // Definisci i due assi Y
+                    id: 'orders',
+                    type: 'linear',
+                    position: 'left',
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }, {
+                    id: 'sales',
+                    type: 'linear',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-      }
-    }
-  });
+    });
 </script>
+
+
 @endsection
