a5b12a3ff5187e747a246c65e02e94dedc6c7a31
FraBona <francescobonafede08@gmail.com>
2024-02-16 16:03:01 +0100

    add send data to table order

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -18,13 +18,14 @@ class OrderController extends Controller
     {
         try {
             $request->validate([
-                'restaurant_id' => 'required|integer', 
+                'restaurant_id' => 'required|integer',
                 'customer_name' => 'required|regex:/[a-zA-Z\s]+/|min:3|max:30|string',
                 'customer_last_name' => 'required|regex:/[a-zA-Z\s]+/|min:3|max:30|string',
                 'customer_address' => 'required|min:10|max:255|string',
                 'customer_email' => 'required|email',
+                'customer_phone' => 'required|regex:/[0-9]+/|min:9|max:10|string',
                 'amount' => 'required|numeric|between:0.5,9999.99',
-                'dishes' => 'required|array', 
+                'dishes' => 'required|array',
                 'dishes.*.dish_id' => 'required|integer',
                 'dishes.*.quantity' => 'required|integer|min:1|max:100'
             ]);
@@ -34,13 +35,13 @@ class OrderController extends Controller
                 'errors' => $e->errors()
             ], 422);
         }
-    
+
 
         $order = Order::create($request->except('dishes'));
 
         $dishes = $request->input('dishes');
 
-        foreach($dishes as $dish){
+        foreach ($dishes as $dish) {
             // Aggiungi il piatto all'ordine utilizzando il metodo attach()
             $order->dishes()->attach($dish['dish_id'], ['quantity' => $dish['quantity']]);
         }
@@ -51,96 +52,78 @@ class OrderController extends Controller
         ]);
     }
 
-    public function savePaymentData(Request $request) {
-
-        // DATI UTENTE 
-
-        // $customer_name = $request->input('user')['firstName'];
-        // $customer_last_name = $request->input('user')['lastName']; 
-        // $customer_email = $request->input('user')['email']; 
-        // $customer_address = $request->input('user')['address']; 
-        // $customer_phone= $request->input('user')['phoneNumber']; 
-        // $restaurant_id = $request->input('chest')['restaurant_id']; 
-
-
-        $error = false; 
+    public function savePaymentData(Request $request)
+    {
+        $error = false;
         $amount = 0;
 
         foreach ($request->input('chest') as $dish) {
-
-            // $dish_id = $dish['id'];
-            // $quantity = $dish['counter'];
-            // $restaurant_id = $dish['restaurant_id'];
-            
-
             $dish_data = [
                 'dish_id' => $dish['id'],
                 'quantity' => $dish['counter'],
-                'restaurant_id' => $dish['restaurant_id']
             ];
-            
 
             $dish_validation_rules = [
                 'dish_id' => 'required|integer',
                 'quantity' => 'required|integer|max:99',
-                'restaurant_id' => 'required|integer'
             ];
 
-            $cart_validation = Validator::make($dish_data,$dish_validation_rules);
+            $cart_validation = Validator::make($dish_data, $dish_validation_rules);
 
-            if ( $cart_validation->fails() ) {
+            if ($cart_validation->fails()) {
                 $error = true;
+                break; // Se c'è un errore, interrompi il ciclo
             } else {
+                // Se non ci sono errori, calcola l'importo totale
                 $dish = Dish::find($dish_data['dish_id']);
                 $dish_price = $dish->price;
                 $dish_quantity = $dish_data['quantity'];
-                $amount += ($dish_price * $dish_quantity );
+                $amount += ($dish_price * $dish_quantity);
             }
-
         }
 
+        // Dati dell'utente
         $user_data = [
             'customer_name' => $request->input('user')['firstName'],
-            'customer_last_name' => $request->input('user')['lastName'], 
-            'customer_email' => $request->input('user')['email'], 
-            'customer_address' => $request->input('user')['address'], 
-            'customer_phone' => $request->input('user')['phoneNumber'], 
-            'restaurant_id' => $request->input('chest')['restaurant_id'],
-            'amount'  => $amount
+            'customer_last_name' => $request->input('user')['lastName'],
+            'customer_email' => $request->input('user')['email'],
+            'customer_address' => $request->input('user')['address'],
+            'customer_phone' => $request->input('user')['phoneNumber'],
+            'restaurant_id' => $request->input('chest')[0]['restaurant_id'],
+            'amount'  => $amount,
+            
         ];
+
 
         $user_validation_rules = [
+            'restaurant_id' => 'required|integer',
             'customer_name' => 'required|regex:/[a-zA-Z\s]+/|min:3|max:30|string',
             'customer_last_name' => 'required|regex:/[a-zA-Z\s]+/|min:3|max:30|string',
-            'customer_email' => 'required|email',
             'customer_address' => 'required|min:10|max:255|string',
+            'customer_email' => 'required|email',
             'customer_phone' => 'required|regex:/[0-9]+/|min:9|max:10|string',
-            'restaurant_id' => 'required|integer', 
-            'amount'  => 'required|integer'
+            'amount' => 'required|numeric|between:0.5,9999.99',
+
         ];
 
-        $user_validation = Validator::make($user_data,$user_validation_rules);
+        $user_validation = Validator::make($user_data, $user_validation_rules);
 
-        if ( $user_validation->fails() ) {
+        if ($user_validation->fails()) {
             $error = true;
         }
 
-        
         if (!$error) {
-
-            Order::create($user_data); 
-            
+            $order = Order::create($user_data);
+            foreach ($request->input('chest') as $dish) {
+                $currentDish = Dish::find($dish['id']);
+                $order->dishes()->attach($currentDish->id, ['quantity' => $dish['counter']]);
+            }
         }
 
-  
-        
         return response()->json([
-            'success' => true
+            'success' => !$error,
+            'dishes' => $dish_data
         ]);
-
-        
-        
-
-        // $finalData->save();
     }
+    //->except('dishes')
 }
