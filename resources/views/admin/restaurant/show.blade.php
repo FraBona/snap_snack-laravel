9e43cf23bc2690ddd51322d3bd91495ba10044aa
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-02-17 16:37:01 +0100

    ADD- show minor style fix

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -3,8 +3,7 @@
     <style>
         .button-wrapper {
             display: flex;
-
-            margin-bottom: 25px;
+            padding: 0 60px;
         }
 
         .restaurant-img {
@@ -13,29 +12,26 @@
             border-radius: 1rem;
         }
 
-        .card-photo{
-           display: flex;
-           align-items: center;
+        .card-photo {
+            display: flex;
+            align-items: center;
         }
 
         .card-description {
             display: flex;
             align-items: center;
-            justify-content: flex-start;
+            justify-content: flex-end;
 
         }
 
-
-
         .restaurant-show {
-
-          display: flex;
-          flex-wrap: wrap;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          padding: 50px
-
+            height: calc(100vh - 300px);
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 50px
         }
 
         .infos {
@@ -49,9 +45,10 @@
         .restaurant-card {
             background-color: white;
             display: grid;
-            grid-template-columns: 30% [first]  70% [end];
+            grid-template-columns: 30% [first] 70% [end];
             border-radius: 2rem;
             box-shadow: rgba(128, 128, 128, 0.44) -6px -6px 6px -6px;
+            min-width: 800px;
         }
 
         .rest-ul {
@@ -65,37 +62,30 @@
         }
     </style>
 
-    <section class="restaurant-show ">
-     <div class="button-wrapper">
+    <div class="button-wrapper">
         <a class="btn btn-primary mt-4" href="{{ route('admin.dashboard') }}">Torna alla Home</a>
-     </div>
-
+    </div>
+    <section class="restaurant-show ">
         <div class="restaurant-card">
             <div class="card-photo p-4">
                 @if ($restaurant->photo)
-                <img class="restaurant-img" src="{{ asset('storage/' . $restaurant->photo) }}" alt="photo-restaurant">
-
-            @endif
+                    <img class="restaurant-img" src="{{ asset('storage/' . $restaurant->photo) }}" alt="photo-restaurant">
+                @endif
             </div>
             <div class="card-description p-4">
-              <ul class="  rest-ul d-flex flex-column ">
-                <li class="infos">Il nome del tuo ristorante é: <strong>{{ $restaurant->name }}</strong></li>
-                <li class="infos">L'Indirizzo del tuo ristorante é: <strong>{{ $restaurant->address }}</strong></li>
-                <li class="infos">Il numero del tuo ristorante é: <strong>{{ $restaurant->phone_number }}</strong></li>
-                <li class="infos">La partita iva del tuo ristorante é: <strong>{{ $restaurant->vat }}</strong></li>
-                <li class="infos">Descrizione: <strong>{{ $restaurant->description }}</strong></li>
-
-                <ul class="d-flex justify-content-center gap-2 p-2 ">
-                    @foreach ($restaurant->categories as $category)
-                        <li class="infos badge rounded-pill text-bg-primary">{{ $category->name }}</li>
-                    @endforeach
+                <ul class="  rest-ul d-flex flex-column ">
+                    <li class="infos">Il nome del tuo ristorante é: <strong>{{ $restaurant->name }}</strong></li>
+                    <li class="infos">L'Indirizzo del tuo ristorante é: <strong>{{ $restaurant->address }}</strong></li>
+                    <li class="infos">Il numero del tuo ristorante é: <strong>{{ $restaurant->phone_number }}</strong></li>
+                    <li class="infos">La partita iva del tuo ristorante é: <strong>{{ $restaurant->vat }}</strong></li>
+                    <li class="infos">Descrizione: <strong>{{ $restaurant->description }}</strong></li>
+                    <ul class="d-flex justify-content-center gap-2 p-2 ">
+                        @foreach ($restaurant->categories as $category)
+                            <li class="infos badge rounded-pill text-bg-primary">{{ $category->name }}</li>
+                        @endforeach
+                    </ul>
                 </ul>
-
-
-            </ul>
-        </div>
+            </div>
         </div>
     </section>
-
-    </html>
 @endsection
